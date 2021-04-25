5d99704a4f5a5f919ae8d11e9ad04d7edb672568
javaComSci <iramanat@purdue.edu>
2021-04-25 12:42:29 -0400

    css loader

--- webpack-demo-app/webpack.config.js
+++ webpack-demo-app/webpack.config.js
@@ -7,5 +7,14 @@ module.exports = {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
     },
-
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                // css-loader takes the css and makes into js
+                // style-loader takes the js and injects into the dom
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    }
 }
